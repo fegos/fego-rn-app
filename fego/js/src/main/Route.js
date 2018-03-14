cb1251c507e247146055abd8c219543588826b35
Dong <zhaosong_204@163.com>
2018-03-14 14:09:47 +0800

    MOD: eslint

--- fego/js/src/main/Route.js
+++ fego/js/src/main/Route.js
@@ -16,17 +16,25 @@ import { Icon } from 'fego-rn';
 // }
 // const TabIcon = GetTabIcon();
 
-/* eslint react/prefer-stateless-function: "off" */
-class TabIcon extends React.Component {
-  render() {
-    const { name, iconAttr } = this.props;
-    // tabBarIcon的父标签必须设置高度以避免icon与label重叠，bug详见： https://github.com/react-community/react-navigation/issues/2805
-    return (
-      <View style={{ height: 26 }}>
-        <Icon name={name} family="nsip" style={{ color: iconAttr.tintColor, fontSize: 22 }} />
-      </View>);
-  }
-}
+// /* eslint react/prefer-stateless-function: "off" */
+// class TabIcon extends React.Component {
+//   render() {
+//     const { name, iconAttr } = this.props;
+//     // tabBarIcon的父标签必须设置高度以避免icon与label重叠，bug详见： https://github.com/react-community/react-navigation/issues/2805
+//     return (
+//       <View style={{ height: 26 }}>
+//         <Icon name={name} family="nsip" style={{ color: iconAttr.tintColor, fontSize: 22 }} />
+//       </View>);
+//   }
+// }
+const TabIcon = (props) => {
+  const { name, iconAttr } = props;
+  // tabBarIcon的父标签必须设置高度以避免icon与label重叠，bug详见： https://github.com/react-community/react-navigation/issues/2805
+  return (
+    <View style={{ height: 26 }}>
+      <Icon name={name} family="nsip" style={{ color: iconAttr.tintColor, fontSize: 22 }} />
+    </View>);
+};
 
 export default {
   Home: {
